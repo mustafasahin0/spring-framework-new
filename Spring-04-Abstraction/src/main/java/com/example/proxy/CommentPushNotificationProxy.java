2eba92afbab362dcc49d6c1a8b16c8512daea955
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-09-10 09:19:56 -0600

    Abstraction

---
+++ Spring-04-Abstraction/src/main/java/com/example/proxy/CommentPushNotificationProxy.java
@@ -0,0 +1,17 @@
+package com.example.proxy;
+
+import com.example.model.Comment;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Primary
+@Component
+@Qualifier("PUSH")
+public class CommentPushNotificationProxy implements CommentNotificationProxy{
+
+    @Override
+    public void sendComment(Comment comment) {
+        System.out.println("Sending push notification for comment: " + comment.getText());
+    }
+}
---
+++ Spring-04-Abstraction/src/main/java/com/example/repository/DBCommentRepository.java
@@ -0,0 +1,13 @@
+package com.example.repository;
+
+import com.example.model.Comment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DBCommentRepository implements CommentRepository {
+
+    @Override
+    public void storeComment(Comment comment) {
+        System.out.println("Storing comment: " + comment.getText());
+    }
+}
