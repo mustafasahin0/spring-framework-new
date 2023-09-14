00cff1eafe8ed5bed03466eac117276f6b3426ac
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-09-14 17:07:50 -0600

    Spring MVC Controller module added

---
+++ Spring-07-Spring-MVC-Controller/src/main/java/com/example/spring07springmvccontroller/Spring07SpringMvcControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring07springmvccontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring07SpringMvcControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring07SpringMvcControllerApplication.class, args);
+	}
+
+}
