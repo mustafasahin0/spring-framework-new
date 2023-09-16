b2c0255d39f4f7df17f016a4b924e03813d62ec4
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-09-16 17:15:25 -0600

    added Modules to StudentController.java

--- Spring-08-Spring-MVC-Model/src/main/java/com/example/controller/StudentController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/example/controller/StudentController.java
@@ -1,13 +1,19 @@
 package com.example.controller;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Configuration // This tells Spring that this is a configuration class
+@Controller // This tells Spring that this class is a controller
 public class StudentController {
 
     @RequestMapping("/welcome") // This tells Spring to map this method to the URL
-    public String homePage() {
+    public String homePage(Model model) {
+
+        model.addAttribute("name", "Mustafa"); // This adds a key-value pair to the model object
+        model.addAttribute("course", "Spring-MVC");
+
         return "/student/welcome";
     }
 }
