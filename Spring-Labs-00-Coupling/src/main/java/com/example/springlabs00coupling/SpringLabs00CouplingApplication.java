0a612f6b9c55200d4e0e2c57b3ac58eaf0badcb5
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-09-17 12:52:24 -0600

    added lab 00 coupling module

---
+++ Spring-Labs-00-Coupling/src/main/java/com/example/springlabs00coupling/SpringLabs00CouplingApplication.java
@@ -0,0 +1,13 @@
+package com.example.springlabs00coupling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLabs00CouplingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLabs00CouplingApplication.class, args);
+	}
+
+}
---
+++ Spring-Labs-00-Coupling/src/test/java/com/example/springlabs00coupling/SpringLabs00CouplingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springlabs00coupling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLabs00CouplingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
