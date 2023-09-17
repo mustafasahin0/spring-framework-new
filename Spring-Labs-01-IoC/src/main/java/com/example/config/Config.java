e1c5b45800069db44754598f5f391b179ef0a45b
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-09-17 16:14:42 -0600

    added IoC lab solution

--- Spring-Labs-01-IoC/src/main/java/com/example/config/Config.java
+++ Spring-Labs-01-IoC/src/main/java/com/example/config/Config.java
@@ -1,4 +1,45 @@
 package com.example.config;
 
+import com.example.Currency;
+import com.example.account.Current;
+import com.example.account.Saving;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Configuration
 public class Config {
+
+    @Bean
+    public Current currentBean(Currency currency){
+        Current current = new Current();
+        current.setAccountId(UUID.randomUUID());
+        current.setAmount(new BigDecimal(250));
+
+        current.setCurrency(currency);
+        return current;
+    }
+
+    @Bean
+    public Saving savingBean(){
+        Saving saving = new Saving();
+
+        saving.setAccountId(UUID.randomUUID());
+        saving.setAmount(new BigDecimal(500));
+
+        saving.setCurrency(currency());
+        return saving;
+    }
+
+    @Bean
+    public Currency currency(){
+        Currency currency = new Currency();
+        currency.setCode("USD");
+        currency.setName("Dollar");
+
+        return currency;
+    }
 }
