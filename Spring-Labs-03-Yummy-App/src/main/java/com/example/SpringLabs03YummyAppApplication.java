983709528a4202b93139f27685736656f7e8ab69
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-09-18 20:15:15 -0600

    yummy app module added

---
+++ Spring-Labs-03-Yummy-App/src/main/java/com/example/SpringLabs03YummyAppApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLabs03YummyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLabs03YummyAppApplication.class, args);
+    }
+
+}
