f19e7bea05d0a39d5645671749cc139fb21e368e
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-09-20 20:27:20 -0600

    Spring Thymeleaf - some modules are added

---
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/example/bootstrap/DataGenerator.java
@@ -0,0 +1,21 @@
+package com.example.bootstrap;
+
+import com.example.model.Student;
+import net.datafaker.Faker;
+
+import java.util.List;
+
+public class DataGenerator {
+
+    public static List<Student> createStudent() {
+        Faker faker = new Faker();
+        List<Student> students = List.of(
+                new Student(faker.name().firstName(), faker.name().lastName(), faker.number().numberBetween(20, 50), faker.address().state()),
+                new Student(faker.name().firstName(), faker.name().lastName(), faker.number().numberBetween(20, 50), faker.address().state()),
+                new Student(faker.name().firstName(), faker.name().lastName(), faker.number().numberBetween(20, 50), faker.address().state()),
+                new Student(faker.name().firstName(), faker.name().lastName(), faker.number().numberBetween(20, 50), faker.address().state()),
+                new Student(faker.name().firstName(), faker.name().lastName(), faker.number().numberBetween(20, 50), faker.address().state())
+        );
+        return students;
+    }
+}
