1f8488d4e7999ff9e09c1d87cf8847a53836f825
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-09-30 17:07:10 -0600

    added mentor class and view

--- Spring-09-Spring-Thymeleaf/src/main/java/com/example/controller/MentorController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/example/controller/MentorController.java
@@ -4,6 +4,8 @@ import com.example.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -18,5 +20,13 @@ public class MentorController {
         List<String> batchList = List.of("JD1", "JD2", "EU1", "EU2");
         model.addAttribute("batchList", batchList);
         return "mentor/mentor-register";
+    }
+
+    @PostMapping("/confirm")
+    public String showForm2(@ModelAttribute("mentor") Mentor mentor) {
+        System.out.println(mentor.toString());
+//        return "mentor/mentor-confirmation";
+
+        return "redirect:/mentor/register";
     }
 }
