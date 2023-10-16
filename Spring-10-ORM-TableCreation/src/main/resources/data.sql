8258cd65757ac516c0e493e49ebd37237f71cc53
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-10-15 21:28:14 -0600

    ORM Part-2

--- Spring-10-ORM-TableCreation/src/main/resources/data.sql
+++ Spring-10-ORM-TableCreation/src/main/resources/data.sql
@@ -1,3 +1,6 @@
 
-INSERT INTO EMPLOYEES(id,name)
-VALUES(1,'Mike Smith');
+-- INSERT INTO EMPLOYEES(id,name)
+-- VALUES(1,'Mike Smith');
+
+INSERT INTO student(id,first_name, last_name, email)
+VALUES(1,'Mike','Smith','mike@example.com');
