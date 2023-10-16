f636b11a4c71efe1e0a4047a82b1cd1739d88e83
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-10-15 23:44:55 -0600

    ORM Part-2

--- Spring-10-ORM-TableCreation/src/main/java/com/example/entity/Student.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/example/entity/Student.java
@@ -1,14 +1,35 @@
 package com.example.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import com.example.enums.Gender;
+import jakarta.persistence.*;
 
-@Entity
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity // this class is an entity
+@Table(name = "students") // if you want to change the name of the table in the db
 public class Student {
 
-    @Id
+    @Id // primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto increment
     private Long id;
+    @Column(name = "studentFirstName")
     private String firstName;
+    @Column(name = "studentLastName") // if you want to change the name of the column in the db
     private String lastName;
     private String email;
+
+    @Transient // not saved in db
+    private String city;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate birthDate; // Add some comment?
+    @Column(columnDefinition = "TIME") // TIME is a sql type, not a java type. so we need to specify the columnDefinition to tell the db what type it is.
+    private LocalTime birthTime;
+    @Column(columnDefinition = "TIMESTAMP") // TIMESTAMP is a sql type, not a java type. so we need to specify the columnDefinition to tell the db what type it is.
+    private LocalTime birthDateTime;
+
+    @Enumerated(EnumType.STRING) // tells the db to save the enum as a string
+    private Gender gender;
+
 }
--- Spring-10-ORM-TableCreation/src/main/resources/data.sql
+++ Spring-10-ORM-TableCreation/src/main/resources/data.sql
@@ -2,5 +2,5 @@
 -- INSERT INTO EMPLOYEES(id,name)
 -- VALUES(1,'Mike Smith');
 
-INSERT INTO student(id,first_name, last_name, email)
-VALUES(1,'Mike','Smith','mike@example.com');
+INSERT INTO students(first_name, last_name, email)
+VALUES('Mike','Smith','mike@example.com');
