beb31c970832a531a0621f899561fb0c2e0588e9
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-10-15 23:54:04 -0600

    ORM Part-2

--- Spring-10-ORM-TableCreation/src/main/java/com/example/entity/Employee.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/example/entity/Employee.java
@@ -1,12 +1,15 @@
 package com.example.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Employee {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
 }
