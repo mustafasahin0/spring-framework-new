3092fcb246cd7736ecc5efc114c7debe215d03de
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-10-16 00:11:48 -0600

    ORM Part-2

---
+++ Spring-10-ORM-TableCreation/src/main/java/com/example/entity/Car.java
@@ -0,0 +1,14 @@
+package com.example.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String make;
+    private String model;
+}
