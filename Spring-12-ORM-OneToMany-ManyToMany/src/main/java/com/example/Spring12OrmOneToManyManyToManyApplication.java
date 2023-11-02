0579a84f8ba0c51813d63322c886ddee931f0a94
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-11-01 19:46:37 -0600

    ORM Part-4

---
+++ Spring-12-ORM-OneToMany-ManyToMany/src/main/java/com/example/Spring12OrmOneToManyManyToManyApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring12OrmOneToManyManyToManyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring12OrmOneToManyManyToManyApplication.class, args);
+    }
+
+}
