cbf8bdba7bb65f2ee99889ae397e29f85f9c079f
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-11-04 21:54:28 -0600

    CinemaLab

---
+++ Spring-13-ORM-CinemaLab/src/main/java/com/example/entity/Account.java
@@ -0,0 +1,12 @@
+package com.example.entity;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Account extends BaseEntity {
+
+}
--- Spring-13-ORM-CinemaLab/src/main/java/com/example/entity/User.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/example/entity/User.java
@@ -1,7 +1,6 @@
 package com.example.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,4 +9,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Table(name = "user_account")
 public class User extends BaseEntity {
+
+    private String email;
+    private String password;
+    private String username;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_details_id")
+    private Account account;
 }
