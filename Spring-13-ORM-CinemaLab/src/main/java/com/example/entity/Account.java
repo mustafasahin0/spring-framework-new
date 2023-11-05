366bb8780e43018efc8fdcd58c47009ce84e126b
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-11-04 22:12:49 -0600

    CinemaLab

--- Spring-13-ORM-CinemaLab/src/main/java/com/example/entity/Account.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/example/entity/Account.java
@@ -1,12 +1,27 @@
 package com.example.entity;
 
-import jakarta.persistence.Entity;
+import com.example.enums.UserRole;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
+@Table(name = "account_details")
 public class Account extends BaseEntity {
 
+    private String name;
+    private String address;
+    private String country;
+    private String city;
+    private String state;
+    private Integer age;
+    private String postalCode;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.USER;
+
+    @OneToOne(mappedBy = "account")
+    private User user;
 }
