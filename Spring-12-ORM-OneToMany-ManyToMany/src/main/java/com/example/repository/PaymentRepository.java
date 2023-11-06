b3ede837aad5ec88ada49b248f278f6ff9866545
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-11-05 18:42:02 -0700

    CinemaLab

--- Spring-12-ORM-OneToMany-ManyToMany/src/main/java/com/example/repository/PaymentRepository.java
+++ Spring-12-ORM-OneToMany-ManyToMany/src/main/java/com/example/repository/PaymentRepository.java
@@ -4,4 +4,9 @@ import com.example.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    // derive query = select * from payment where id = ?
+
+    // sql @Query
+
+    // JPQL @Query
 }
