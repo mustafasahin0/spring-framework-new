8f4ac9b3d22d5b81d8ada7181e3da4eb572dc2fb
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-11-25 22:51:52 -0700

    Cinemalab: Entities Updated

--- Spring-13-ORM-CinemaLab/src/main/java/com/example/repository/AccountRepository.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/example/repository/AccountRepository.java
@@ -15,17 +15,17 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     // ------------------- DERIVED QUERIES ------------------- //
 
     //Write a derived query to list all accounts with a specific country or state
-    List<Account> findByCountryOrState(String country, String state);
+    List<Account> findAllByCountryOrState(String country, String state);
     //Write a derived query to list all accounts with age lower than or equal to a specific value
-    List<Account> findByAgeLessThanEqual(int age);
+    List<Account> findAllByAgeLessThanEqual(int age);
     //Write a derived query to list all accounts with a specific role
-    List<Account> findByRole(UserRole userRole);
+    List<Account> findAllByRole(UserRole userRole);
     //Write a derived query to list all accounts between a range of ages
-    List<Account> findByAgeBetween(int age1, int age2);
+    List<Account> findAllByAgeBetween(int age1, int age2);
     //Write a derived query to list all accounts where the beginning of the address contains the keyword
-    List<Account> findByAddressIsStartingWith(String pattern);
+    List<Account> findAllByAddressStartingWith(String pattern);
     //Write a derived query to sort the list of accounts with age
-    List<Account> findAllOrderByAge();
+    List<Account> findAllByOOrderByAgeDesc();
     // ------------------- JPQL QUERIES ------------------- //
 
     //Write a JPQL query that returns all accounts
@@ -41,13 +41,9 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     // ------------------- Native QUERIES ------------------- //
 
     //Write a native query to read all accounts with an age lower than a specific value
-    @Query(value = "SELECT * FROM ACCOUNTS WHERE AGE <= :age", nativeQuery = true)
+    @Query(value = "SELECT * FROM account_details WHERE age < :age", nativeQuery = true)
     List<Account> getAccountsLessThanAge(@Param("age") int age);
     //Write a native query to read all accounts that a specific value can be containable in the name, address, country, state city
-    @Query(value = "SELECT * FROM ACCOUNTS WHERE NAME LIKE :pattern OR ADDRESS LIKE :pattern OR COUNTRY LIKE :pattern OR STATE LIKE :pattern OR CITY LIKE :pattern", nativeQuery = true)
+    @Query(value = "SELECT * FROM ACCOUNT_DETAIlS WHERE NAME ILIKE concat('%', ?1, '%') OR ADDRESS ILIKE concat('%', ?1, '%') OR COUNTRY ILIKE concat('%', ?1, '%') OR STATE ILIKE concat('%', ?1, '%') OR CITY ILIKE concat('%', ?1, '%')", nativeQuery = true)
     List<Account> getAllAccountsContainsNameAddressCountryStateCity(@Param("pattern") String pattern);
-    //Write a native query to read all accounts with an age lower than a specific value
-    @Query(value = "SELECT * FROM ACCOUNTS WHERE AGE <= :age", nativeQuery = true)
-    List<Account> getAllAccountsLessThanAge(@Param("age") int age);
-
 }
