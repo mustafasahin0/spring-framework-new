0571d989969785565cd97cdf7bd745514e35ab3c
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-11-28 17:39:23 -0700

    Cinemalab: Entities Updated

--- Spring-13-ORM-CinemaLab/src/main/java/com/example/repository/AccountRepository.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/example/repository/AccountRepository.java
@@ -16,14 +16,19 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
 
     //Write a derived query to list all accounts with a specific country or state
     List<Account> findAllByCountryOrState(String country, String state);
+
     //Write a derived query to list all accounts with age lower than or equal to a specific value
     List<Account> findAllByAgeLessThanEqual(int age);
+
     //Write a derived query to list all accounts with a specific role
     List<Account> findAllByRole(UserRole userRole);
+
     //Write a derived query to list all accounts between a range of ages
     List<Account> findAllByAgeBetween(int age1, int age2);
+
     //Write a derived query to list all accounts where the beginning of the address contains the keyword
     List<Account> findAllByAddressStartingWith(String pattern);
+
     //Write a derived query to sort the list of accounts with age
     List<Account> findAllByOOrderByAgeDesc();
     // ------------------- JPQL QUERIES ------------------- //
@@ -31,9 +36,11 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     //Write a JPQL query that returns all accounts
     @Query("SELECT a FROM Account a")
     List<Account> getAllAccounts();
+
     //Write a JPQL query to list all admin accounts
     @Query("SELECT a FROM Account a WHERE a.role = 'ADMIN'")
     List<Account> getAllAdmins();
+
     //Write a JPQL query to sort all accounts with age
     @Query("SELECT a FROM Account a ORDER BY a.age")
     List<Account> sortAllByAges();
@@ -43,6 +50,7 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     //Write a native query to read all accounts with an age lower than a specific value
     @Query(value = "SELECT * FROM account_details WHERE age < :age", nativeQuery = true)
     List<Account> getAccountsLessThanAge(@Param("age") int age);
+
     //Write a native query to read all accounts that a specific value can be containable in the name, address, country, state city
     @Query(value = "SELECT * FROM ACCOUNT_DETAIlS WHERE NAME ILIKE concat('%', ?1, '%') OR ADDRESS ILIKE concat('%', ?1, '%') OR COUNTRY ILIKE concat('%', ?1, '%') OR STATE ILIKE concat('%', ?1, '%') OR CITY ILIKE concat('%', ?1, '%')", nativeQuery = true)
     List<Account> getAllAccountsContainsNameAddressCountryStateCity(@Param("pattern") String pattern);
--- Spring-13-ORM-CinemaLab/src/main/java/com/example/repository/CinemaRepository.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/example/repository/CinemaRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CinemaRepository extends JpaRepository<Cinema, Long> {
@@ -14,30 +15,36 @@ public interface CinemaRepository extends JpaRepository<Cinema, Long> {
     // ------------------- DERIVED QUERIES ------------------- //
 
     //Write a derived query to get cinema with a specific name
-    List<Cinema> getAllByName(String name);
+    Optional<Cinema> getAllByName(String name);
+
     //Write a derived query to read sorted the top 3 cinemas that contains a specific sponsored name
-    List<Cinema> getTop3BySponsoredNameContaining(String pattern);
+    List<Cinema> getTop3BySponsoredNameContainingOrderBySponsoredName(String pattern);
+
     //Write a derived query to list all cinemas in a specific country
-    List<Cinema> getAllByLocationCountry(String country);
+    List<Cinema> getAllByLocation_Country(String country);
+
     //Write a derived query to list all cinemas with a specific name or sponsored name
     List<Cinema> getAllByNameOrSponsoredName(String name, String sponsoredName);
     // ------------------- JPQL QUERIES ------------------- //
 
     //Write a JPQL query to read the cinema name with a specific id
-    @Query(value = "SELECT c FROM Cinema c WHERE c.name =:name AND c.id = :id")
-    List<Cinema> getAllByNameWithId(@Param("name") String name, @Param("id") Long id);
+    @Query("SELECT c.name FROM Cinema c WHERE c.id = :id")
+    String fetchById(@Param("id") Long id);
     // ------------------- Native QUERIES ------------------- //
 
     //Write a native query to read all cinemas by location country
-    @Query(value = "SELECT * FROM CINEMA c INNER JOIN LOCATION l ON c.location_id = l.id WHERE l.country = :country", nativeQuery = true)
-    List<Cinema> getAllCinemasByLocationCountry(@Param("country") String country);
+    @Query(value = "SELECT * FROM CINEMA c JOIN LOCATION l ON c.location_id = l.id WHERE l.country = ?1", nativeQuery = true)
+    List<Cinema> getAllCinemasByLocationCountry(String country);
+
     //Write a native query to read all cinemas by name or sponsored name contains a specific pattern
-    @Query(value = "SELECT * FROM CINEMA WHERE name LIKE %:name% OR sponsored_name LIKE %:pattern%", nativeQuery = true)
-    List<Cinema> getAllCinemasByNameOrSponsoredNameContaining(@Param("name") String name, @Param("pattern") String pattern);
+    @Query(value = "SELECT * FROM CINEMA WHERE name ILIKE concat('%', ?1, '%') OR sponsored_name ILIKE concat('%', ?1, '%')", nativeQuery = true)
+    List<Cinema> getAllCinemasByNameOrSponsoredNameContaining(String pattern);
+
     //Write a native query to sort all cinemas by name
-    @Query(value = "SELECT * FROM CINEMA c ORDER BY c.name", nativeQuery = true)
+    @Query(value = "SELECT * FROM CINEMA ORDER BY name", nativeQuery = true)
     List<Cinema> sortAllByName();
+
     //Write a native query to distinct all cinemas by sponsored name
-    @Query(value = "SELECT DISTINCT FROM CINEMA WHERE sponsored_name IS LIKE :name", nativeQuery = true)
-    List<Cinema> getAllDistinctName(@Param("name") String name);
+    @Query(value = "SELECT DISTINCT SPONSORED_NAME FROM CINEMA", nativeQuery = true)
+    List<Cinema> getAllDistinctName();
 }
