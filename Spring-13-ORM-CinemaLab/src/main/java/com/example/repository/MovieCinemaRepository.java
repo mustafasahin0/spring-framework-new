7bb6b43296cf2c407236bd0af8072cded2f61b6d
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-12-05 15:01:20 -0700

    Cinemalab: Entities Updated

--- Spring-13-ORM-CinemaLab/src/main/java/com/example/repository/MovieCinemaRepository.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/example/repository/MovieCinemaRepository.java
@@ -18,16 +18,22 @@ public interface MovieCinemaRepository extends JpaRepository<MovieCinema, Long> 
 
     //Write a derived query to read movie cinema with id
     Optional<MovieCinema> findById(Long id);
+
     //Write a derived query to count all movie cinemas with a specific cinema id
     Integer countAllByCinemaId(Long id);
+
     //Write a derived query to count all movie cinemas with a specific movie id
     List<MovieCinema> findAllByMovie(Movie movie);
+
     //Write a derived query to list all movie cinemas with higher than a specific date
     List<MovieCinema> findAllByDateTimeAfter(LocalDateTime localDateTime);
+
     //Write a derived query to find the top 3 expensive movies
     List<MovieCinema> findTop3ByOrderByMoviePriceDesc();
+
     //Write a derived query to list all movie cinemas that contain a specific movie name
     List<MovieCinema> findAllByMovieNameContaining(String movieName);
+
     //Write a derived query to list all movie cinemas in a specific location
     List<MovieCinema> findAllByCinema_Location_Name(String name);
     // ------------------- JPQL QUERIES ------------------- //
@@ -38,9 +44,10 @@ public interface MovieCinemaRepository extends JpaRepository<MovieCinema, Long> 
     // ------------------- Native QUERIES ------------------- //
 
     //Write a native query to count all movie cinemas by cinema id
-    @Query(value = "SELECT count(*) FROM MOVIE_CINEMA", nativeQuery = true)
-    List<MovieCinema> countAllMovieCinemas();
+    @Query(value = "SELECT count(*) FROM MOVIE_CINEMA WHERE cinema_id = ?1", nativeQuery = true)
+    List<MovieCinema> countAllMovieCinemas(Long cinemaId);
+
     //Write a native query that returns all movie cinemas by location name
-    @Query(value = "SELECT * FROM MOVIE_CINEMA ms INNER JOIN LOCATION l ON ms.location_id = l.id WHERE l.name = :locationName", nativeQuery = true)
+    @Query(value = "SELECT * FROM MOVIE_CINEMA mc JOIN cinema c ON mc.cinema_id = c.id JOIN location l on c.location_id = l.id WHERE l.name=?1", nativeQuery = true)
     List<MovieCinema> getAllByLocationName(@Param("locationName") String locationName);
 }
