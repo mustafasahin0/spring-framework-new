73e7d54e8421a3db2b94b76f6412bdae732c7b1d
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-12-17 00:17:38 -0700

    added Jackson module

---
+++ Spring-16-REST-Jackson/src/test/java/org/example/spring16restjackson/Spring16RestJacksonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spring16restjackson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring16RestJacksonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
