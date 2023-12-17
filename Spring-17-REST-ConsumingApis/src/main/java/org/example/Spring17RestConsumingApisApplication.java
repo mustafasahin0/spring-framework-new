41105b15ba68abe5919d9c2a2ae9d5a51dbe74cd
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-12-18 00:24:55 +0100

    Rest Consuming

--- Spring-17-REST-ConsumingApis/src/main/java/org/example/Spring17RestConsumingApisApplication.java
+++ Spring-17-REST-ConsumingApis/src/main/java/org/example/Spring17RestConsumingApisApplication.java
@@ -1,4 +1,4 @@
-package org.example.spring17restconsumingapis;
+package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
