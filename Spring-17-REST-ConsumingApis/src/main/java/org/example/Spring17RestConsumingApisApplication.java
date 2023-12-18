b4b9b1443e69afe1a234e011e0a6bbafccf52bcd
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-12-18 08:43:36 +0100

    Rest Consuming

--- Spring-17-REST-ConsumingApis/src/main/java/org/example/Spring17RestConsumingApisApplication.java
+++ Spring-17-REST-ConsumingApis/src/main/java/org/example/Spring17RestConsumingApisApplication.java
@@ -2,6 +2,8 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Spring17RestConsumingApisApplication {
@@ -10,4 +12,8 @@ public class Spring17RestConsumingApisApplication {
         SpringApplication.run(Spring17RestConsumingApisApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
---
+++ Spring-17-REST-ConsumingApis/src/main/java/org/example/controller/Consume_RestTemplate.java
@@ -0,0 +1,31 @@
+package org.example.controller;
+
+import org.example.dto.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class Consume_RestTemplate {
+    private final String URI = "https://jsonplaceholder.typicode.com/users";
+
+    private final RestTemplate restTemplate;
+
+    public Consume_RestTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @GetMapping
+    public User[] readAllUsers() {
+        ResponseEntity<User[]> responseEntity =  restTemplate.getForEntity(URI, User[].class);
+
+
+        return responseEntity.getBody();
+    }
+}
