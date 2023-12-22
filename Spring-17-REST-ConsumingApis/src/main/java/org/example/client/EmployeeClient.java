e5608ab6df687d917401b24126052c7c77256d6d
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-12-22 09:38:11 +0300

    Rest Consuming

--- Spring-17-REST-ConsumingApis/src/main/java/org/example/client/EmployeeClient.java
+++ Spring-17-REST-ConsumingApis/src/main/java/org/example/client/EmployeeClient.java
@@ -2,11 +2,12 @@ package org.example.client;
 import org.example.dto.Employee;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(url = "https://dummyapi.io", name = "EMPLOYEE-CLIENT")
 public interface EmployeeClient {
 
     @GetMapping("/data/v1/user?limit=10")
-    Employee getEmployee();
+    Employee getEmployee(@RequestHeader("app-id") String id);
 
 }
--- Spring-17-REST-ConsumingApis/src/main/java/org/example/controller/Consume_FeignClient.java
+++ Spring-17-REST-ConsumingApis/src/main/java/org/example/controller/Consume_FeignClient.java
@@ -1,6 +1,7 @@
 package org.example.controller;
 
 import org.apache.catalina.filters.AddDefaultCharsetFilter;
+import org.example.client.EmployeeClient;
 import org.example.client.UserClient;
 import org.example.dto.ResponseWrapper;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -14,15 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class Consume_FeignClient {
 
     private final UserClient userClient;
+    private final EmployeeClient employeeClient;
 
-    public Consume_FeignClient(UserClient userClient) {
+    public Consume_FeignClient(UserClient userClient, EmployeeClient employeeClient) {
         this.userClient = userClient;
+        this.employeeClient = employeeClient;
     }
 
     @GetMapping("/api/v1/users")
     public ResponseEntity<ResponseWrapper> getUsers() {
 
-        return ResponseEntity.ok(new ResponseWrapper("UserLitst Retirieved", userClient.getUsers()));
+        return ResponseEntity.ok(new ResponseWrapper("UserList Retrieved", userClient.getUsers()));
+    }
+
+    @GetMapping("/api/v1/employee")
+    public ResponseEntity<ResponseWrapper> getEmployee() {
+
+        return ResponseEntity.ok(new ResponseWrapper("Successful", employeeClient.getEmployee("6581035d76db2e63dfd8ba0a")));
     }
 
 }
