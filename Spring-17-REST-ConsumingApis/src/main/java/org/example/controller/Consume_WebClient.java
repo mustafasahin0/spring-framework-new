41342705919f67dee802868356f9f115e2666921
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-12-22 16:07:40 +0300

    Rest Consuming

--- Spring-17-REST-ConsumingApis/src/main/java/org/example/controller/Consume_WebClient.java
+++ Spring-17-REST-ConsumingApis/src/main/java/org/example/controller/Consume_WebClient.java
@@ -1,8 +1,32 @@
 package org.example.controller;
 
+import org.example.entity.MovieCinema;
+import org.example.repository.GenreRepository;
+import org.example.repository.MovieCinemaRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class Consume_WebClient {
+    private final MovieCinemaRepository movieCinemaRepository;
+    private final GenreRepository genreRepository;
 
+    public Consume_WebClient(MovieCinemaRepository movieCinemaRepository, GenreRepository genreRepository) {
+        this.movieCinemaRepository = movieCinemaRepository;
+        this.genreRepository = genreRepository;
+    }
+
+    @GetMapping("/flux-movie-cinema")
+    public Flux<MovieCinema> readAllCinemaFlux() {
+        return Flux.fromIterable(movieCinemaRepository.findAll());
+    }
+
+    @GetMapping("/mono-movie-cinema/{id}")
+    public Mono<MovieCinema> readById(@PathVariable("id") Long id) {
+
+        return Mono.just(movieCinemaRepository.findById(id).get());
+    }
 }
