ecfc70b27b7af818685348058beedc83ca95470c
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-12-28 14:00:43 +0100

    Keycloak

---
+++ Spring-19-REST-Security-Keycloak-LicenseApp/src/test/java/org/example/Spring19RestSecurityKeycloakLicenseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring19RestSecurityKeycloakLicenseAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
