74a4539e7cb4da8353e4cfd700bf3686d0737834
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-12-28 14:08:34 +0100

    Keycloak

--- Spring-19-REST-Security-Keycloak-OrganizationsApp/src/test/java/org/example/Spring19RestSecurityKeycloakOrganizationsAppApplicationTests.java
+++ Spring-19-REST-Security-Keycloak-OrganizationsApp/src/test/java/org/example/Spring19RestSecurityKeycloakOrganizationsAppApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Spring19RestSecurityKeycloakOrganizationsAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
