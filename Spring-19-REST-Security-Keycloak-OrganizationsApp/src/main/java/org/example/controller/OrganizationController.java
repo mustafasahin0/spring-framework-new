478861205865bd5142de209cc697f469b5ae1bec
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-12-31 14:56:34 -0700

    Keycloak

--- Spring-19-REST-Security-Keycloak-OrganizationsApp/src/main/java/org/example/controller/OrganizationController.java
+++ Spring-19-REST-Security-Keycloak-OrganizationsApp/src/main/java/org/example/controller/OrganizationController.java
@@ -1,11 +1,12 @@
 package org.example.controller;
 
-
 import org.example.model.Organization;
 import org.example.service.OrganizationService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.security.RolesAllowed;
 
 @RestController
 @RequestMapping(value = "/v1/organization")
@@ -17,16 +18,19 @@ public class OrganizationController {
         this.organizationService = organizationService;
     }
 
+    @RolesAllowed({"ADMIN", "USER"})
     @GetMapping("/{organizationId}")
     public ResponseEntity<Organization> getOrganization(@PathVariable("organizationId") Long organizationId) throws Exception {
         return ResponseEntity.ok(organizationService.findById(organizationId));
     }
 
+    @RolesAllowed({"ADMIN", "USER"})
     @PostMapping
     public ResponseEntity<Organization> createOrganization(@RequestBody Organization organization) {
         return ResponseEntity.status(HttpStatus.CREATED).body(organizationService.create(organization));
     }
 
+    @RolesAllowed({"ADMIN"})
     @DeleteMapping("/{organizationId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteLicense(@PathVariable("organizationId") Long organizationId) {
