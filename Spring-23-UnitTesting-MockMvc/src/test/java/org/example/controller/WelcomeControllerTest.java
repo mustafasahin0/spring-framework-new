181c7667e69d5f413d85593afc38c05facf8b79f
mustafasahin0 <sahinmustafaenes4@gmail.com>
2024-01-11 10:47:19 -0700

    MockMVC

--- Spring-23-UnitTesting-MockMvc/src/test/java/org/example/controller/WelcomeControllerTest.java
+++ Spring-23-UnitTesting-MockMvc/src/test/java/org/example/controller/WelcomeControllerTest.java
@@ -1,7 +1,47 @@
 package org.example.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(WelcomeController.class)
 class WelcomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void welcome() throws Exception {
+
+        // Call /welcome endpoint
+        // verify "welcome"
+
+        RequestBuilder request = MockMvcRequestBuilders.get("/welcome")
+                .accept(MediaType.APPLICATION_JSON);
+
+        MvcResult mvcResult = mockMvc.perform(request).andReturn();
+        assertEquals(200, mvcResult.getResponse().getStatus());
+        assertEquals("welcome", mvcResult.getResponse().getContentAsString());
+
+    }
+
+    @Test
+    void welcome2() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/welcome")
+                .accept(MediaType.APPLICATION_JSON);
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().string("welcome"))
+                .andReturn();
+    }
+
 
 }
